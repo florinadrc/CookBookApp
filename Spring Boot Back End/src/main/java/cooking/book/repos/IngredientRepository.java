9279ad6d370ae22f1969bd4ec5a78bfbf4ff39e4
florinadrc <43349634+florinadrc@users.noreply.github.com>
2019-01-08 17:32:54 +0000

    Delete IngredientRepository.java

--- Spring Boot Back End/src/main/java/cooking/book/repos/IngredientRepository.java
+++
@@ -1,7 +0,0 @@
-package cooking.book.repos;
-
-import cooking.book.model.Ingredient;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
-}
