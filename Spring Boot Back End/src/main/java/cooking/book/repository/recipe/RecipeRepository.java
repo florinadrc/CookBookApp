aa881afbb6bba79a2a4d7051bed9ce5239b4f108
florinadrc <43349634+florinadrc@users.noreply.github.com>
2019-01-23 14:31:05 +0000

    fixed error after refactor

--- Spring Boot Back End/src/main/java/cooking/book/repository/recipe/RecipeRepository.java
+++ Spring Boot Back End/src/main/java/cooking/book/repository/recipe/RecipeRepository.java
@@ -5,7 +5,7 @@ import cooking.book.model.recipe.RecipeCategory;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-    Iterable<Recipe> findAllByRecipeCategory(RecipeCategory recipeCategory);
-    Iterable<Recipe> findAllByRecipeNameContainingIgnoreCase(String recipeName);
+    Iterable<Recipe> findAllByCategory(RecipeCategory recipeCategory);
+    Iterable<Recipe> findAllByNameContainingIgnoreCase(String recipeName);
     Iterable<Recipe> findAllByOrderByNoOfTimesAccessedDesc();
 }
