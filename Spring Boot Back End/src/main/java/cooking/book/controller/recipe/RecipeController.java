2bf55839269b16bf7737477269c88ee512745a04
florinadrc <43349634+florinadrc@users.noreply.github.com>
2019-01-24 15:11:45 +0000

    added service layer

--- Spring Boot Back End/src/main/java/cooking/book/controller/recipe/RecipeController.java
+++ Spring Boot Back End/src/main/java/cooking/book/controller/recipe/RecipeController.java
@@ -1,17 +1,15 @@
 package cooking.book.controller.recipe;
 
-import cooking.book.repository.recipe.RecipeRepository;
 import cooking.book.model.recipe.Recipe;
-import cooking.book.model.recipe.RecipeCategory;
+import cooking.book.repository.recipe.RecipeRepository;
+import cooking.book.service.recipe.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/recipes")
@@ -19,10 +17,12 @@ import java.util.Optional;
 public class RecipeController {
 
     private RecipeRepository recipeRepository;
+    private RecipeService recipeService;
 
     @Autowired
-    public RecipeController(RecipeRepository recipeRepository){
+    public RecipeController(RecipeRepository recipeRepository, RecipeService recipeService){
         this.recipeRepository = recipeRepository;
+        this.recipeService = recipeService;
     }
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -37,12 +37,7 @@ public class RecipeController {
 
     @GetMapping(value = "/all/byCategory/{theCategory}")
     public List<Recipe> getAllByCategory(@PathVariable String theCategory){
-        RecipeCategory recipeCategory = RecipeCategory.STARTER;
-        if(theCategory.equals("1"))
-            recipeCategory = RecipeCategory.MAIN_COURSE;
-        if(theCategory.equals("2"))
-            recipeCategory = RecipeCategory.DESSERT;
-        return recipeRepository.findAllByRecipeCategory(recipeCategory);
+        return recipeRepository.findAllByRecipeCategory(recipeService.findCategory(theCategory));
     }
 
     @GetMapping(value = "/all/byName/{term}")
@@ -52,8 +47,7 @@ public class RecipeController {
 
     @PostMapping(value = "/create")
     public Recipe postRecipe(@RequestBody Recipe recipe){
-        recipe.setLastAccessed(new Date());
-        recipeRepository.save(recipe);
+        recipeService.newRecipe(recipe);
         return recipe;
     }
 
@@ -68,45 +62,11 @@ public class RecipeController {
 
     @GetMapping(value = "/recipe/{id}")
     public ResponseEntity<Recipe> getRecipe(@PathVariable long id){
-        Recipe recipeToSave = recipeRepository.findById(id).orElse(null);
-        Recipe recipeToReturn = new Recipe();
-
-        if (recipeToSave != null) {
-            recipeToReturn.setRecipeId(recipeToSave.getRecipeId());
-            copyRecipe(recipeToReturn, recipeToSave);
-            recipeToSave.setLastAccessed(new Date());
-            recipeToSave.setNoOfTimesAccessed(recipeToReturn.getNoOfTimesAccessed() + 1);
-
-            recipeRepository.save(recipeToSave);
-
-            return new ResponseEntity<>(recipeToReturn, HttpStatus.OK);
-        }
-
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return recipeService.getRecipe(id);
     }
 
     @PostMapping(value = "/update/{id}")
     public Recipe updateRecipe(@RequestBody Recipe recipe, @PathVariable long id){
-        Optional<Recipe> recipeToCheck = recipeRepository.findById(id);
-
-        if(recipeToCheck.isPresent()){
-            Recipe recipeToSave = recipeToCheck.get();
-            copyRecipe(recipeToSave, recipe);
-
-            recipeRepository.save(recipeToSave);
-            return recipeToSave;
-        } else {
-            return null;
-        }
-    }
-
-    private void copyRecipe(Recipe recipe1, Recipe recipe2) {
-        recipe1.setRecipeName(recipe2.getRecipeName());
-        recipe1.setRecipeCategory(recipe2.getRecipeCategory());
-        recipe1.setIngredientsList(recipe2.getIngredientsList());
-        recipe1.setInstructions(recipe2.getInstructions());
-        recipe1.setSuggestions(recipe2.getSuggestions());
-        recipe1.setLastAccessed(recipe2.getLastAccessed());
-        recipe1.setNoOfTimesAccessed(recipe2.getNoOfTimesAccessed());
+        return recipeService.updateRecipe(recipe, id);
     }
 }
---
+++ Spring Boot Back End/src/main/java/cooking/book/service/recipe/RecipeService.java
@@ -0,0 +1,79 @@
+package cooking.book.service.recipe;
+
+import cooking.book.model.recipe.Recipe;
+import cooking.book.model.recipe.RecipeCategory;
+import cooking.book.repository.recipe.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class RecipeService {
+
+    private RecipeRepository recipeRepository;
+
+    @Autowired
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public RecipeCategory findCategory(String category) {
+        RecipeCategory recipeCategory = RecipeCategory.STARTER;
+        if(category.equals("1"))
+            recipeCategory = RecipeCategory.MAIN_COURSE;
+        if(category.equals("2"))
+            recipeCategory = RecipeCategory.DESSERT;
+
+        return recipeCategory;
+    }
+
+    public void newRecipe(Recipe recipe) {
+        recipe.setLastAccessed(new Date());
+        recipeRepository.save(recipe);
+    }
+
+    public ResponseEntity<Recipe> getRecipe(long id) {
+        Recipe recipeToSave = recipeRepository.findById(id).orElse(null);
+        Recipe recipeToReturn = new Recipe();
+
+        if (recipeToSave != null) {
+            recipeToReturn.setRecipeId(recipeToSave.getRecipeId());
+            copyRecipe(recipeToReturn, recipeToSave);
+            recipeToSave.setLastAccessed(new Date());
+            recipeToSave.setNoOfTimesAccessed(recipeToReturn.getNoOfTimesAccessed() + 1);
+
+            recipeRepository.save(recipeToSave);
+
+            return new ResponseEntity<>(recipeToReturn, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public Recipe updateRecipe(Recipe recipe, long id) {
+        Optional<Recipe> recipeToCheck = recipeRepository.findById(id);
+
+        if (recipeToCheck.isPresent()) {
+            Recipe recipeToSave = recipeToCheck.get();
+            copyRecipe(recipeToSave, recipe);
+
+            recipeRepository.save(recipeToSave);
+            return recipeToSave;
+        } else {
+            return null;
+        }
+    }
+
+    private void copyRecipe(Recipe recipe1, Recipe recipe2) {
+        recipe1.setRecipeName(recipe2.getRecipeName());
+        recipe1.setRecipeCategory(recipe2.getRecipeCategory());
+        recipe1.setIngredientsList(recipe2.getIngredientsList());
+        recipe1.setInstructions(recipe2.getInstructions());
+        recipe1.setSuggestions(recipe2.getSuggestions());
+        recipe1.setLastAccessed(recipe2.getLastAccessed());
+        recipe1.setNoOfTimesAccessed(recipe2.getNoOfTimesAccessed());
+    }
+}
