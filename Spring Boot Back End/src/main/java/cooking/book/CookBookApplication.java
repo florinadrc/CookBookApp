71a14942b5f58e9e4759e723b9ac5c235d6180d7
Florin Coca <florincocajr@yahoo.com>
2019-01-26 13:44:07 +0000

    renamed main class

--- Spring Boot Back End/src/main/java/cooking/book/CookBookApplication.java
+++ Spring Boot Back End/src/main/java/cooking/book/CookBookApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookApplication {
+public class CookBookApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BookApplication.class, args);
+        SpringApplication.run(CookBookApplication.class, args);
     }
 }
